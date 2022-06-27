22339b49fa359e88dc3f4433b86560213514c79d
sungwooklee <1115lsw@gmail.com>
2022-06-27 16:55:39 +0900

    스프링 MVC 검증 Bean Validation edit form에 적용

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
@@ -57,8 +57,7 @@ public class ValidationItemControllerV3 {
         }
 
         //검증에 실패하면 다시 입력 폼으로
-        if(bindingResult.hasErrors())
-        {
+        if(bindingResult.hasErrors()) {
             log.info("bindingResult = {}", bindingResult);
             return "validation/v3/addForm";
         }
@@ -73,14 +72,32 @@ public class ValidationItemControllerV3 {
 
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
+
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
         return "validation/v3/editForm";
     }
 
     @PostMapping("/{itemId}/edit")
-    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+    public String edit(@PathVariable Long itemId, @Validated @ModelAttribute Item item, BindingResult bindingResult) {
+
+        //특정 필드가 아닌 복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+
+        //검증에 실패하면 다시 입력 폼으로
+        if(bindingResult.hasErrors()) {
+            log.info("bindingResult = {}", bindingResult);
+            return "validation/v3/editForm";
+        }
+
         itemRepository.update(itemId, item);
+
+
         return "redirect:/validation/v3/items/{itemId}";
     }
 
